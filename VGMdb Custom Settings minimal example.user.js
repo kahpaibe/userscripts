f41503f06ef53c0bdfa735cc1847a4540757efa8
kahpaibe <85435720+kahpaibe@users.noreply.github.com>
2026-04-15 21:42:19 +0200

    Updated custom settings minimal example

--- VGMdb Custom Settings minimal example.user.js
+++ VGMdb Custom Settings minimal example.user.js
@@ -32,13 +32,37 @@
                         console.log('[VGMdb minimal example] showConsoleLog changed:', value);
                     }
                 }
+                    ,
+                    {
+                        type: 'radio',
+                        id: 'exampleRadio',
+                        label: 'Example radio',
+                        options: [
+                            { value: 'optionA', label: 'Option A' },
+                            { value: 'optionB', label: 'Option B' }
+                        ],
+                        default: 'optionA',
+                        onChange: function(value) {
+                            console.log('[VGMdb minimal example] exampleRadio changed:', value);
+                        }
+                    },
+                    {
+                        type: 'tristate',
+                        id: 'exampleTristate',
+                        label: 'Example tristate',
+                        default: 'null',
+                        onChange: function(value) {
+                            console.log('[VGMdb minimal example] exampleTristate changed:', value);
+                        }
+                    }
             ]
         }
     });
 
     manager.mount();
 
-    if (manager.getSetting('showConsoleLog', false)) {
-        console.log('[VGMdb minimal example] checkbox is enabled');
-    }
+        // Log current values for all controls after mount
+        console.log('[VGMdb minimal example] showConsoleLog:', manager.getSetting('showConsoleLog', false));
+        console.log('[VGMdb minimal example] exampleRadio:', manager.getSetting('exampleRadio', 'optionA'));
+        console.log('[VGMdb minimal example] exampleTristate:', manager.getSetting('exampleTristate', 'null'));
 })();
